b3ec1f369a7296de8b9caa0c3df1959eb726fbaa
Chen Peleg <chenpeleg@gmail.com>
2025-12-03 09:21:29 +0200

    feat: add About link to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"; 
+import Link from "next/link";
 
 export default function Home() { 
  
@@ -10,6 +11,7 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             First - next
           </h1>
+          <Link href={'about'}>About</Link>
        
         </div>
        
