5db831eb2f4ae014830f1a1474928f947df317f1
Chen Peleg <chenpeleg@gmail.com>
2025-12-04 16:44:05 +0200

    feat: create PostsLayout component and update PostsPage to display post name

---
+++ app/posts/layout.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Card} from '@/components/Card';
+import MyHeader from '@/app/header';
+import Link from '@/node_modules/next/link';
+
+export default function PostsLayout({
+                                        children,
+                                    }: Readonly<{
+    children: React.ReactNode;
+}>) {
+
+    return (
+        <div>
+            <h1>Posts Layout</h1>
+
+            (<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+            <main
+                className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <MyHeader/>
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    {children}
+
+                </div>
+
+            </main>
+        </div>);
+        </div>
+    );
+
+}
