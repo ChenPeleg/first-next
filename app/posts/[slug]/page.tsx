1f023672059bf220067d49c214f134d8460f3fec
Chen Peleg <chenpeleg@gmail.com>
2025-12-07 10:33:21 +0200

    feat: add priority attribute to Image component for improved loading performance in PostsPage

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default function PostsPage(props: PageProperties<'/posts/[slug]'>) {
 
     return (<div>
         <h1> {postName.slug}  </h1>
-        <Image src={dumplings } alt={'dumplings'}/>
+        <Image src={dumplings } alt={'dumplings'} priority={true} />
         <p>This is the posts page content.</p>
     </div>);
 }
