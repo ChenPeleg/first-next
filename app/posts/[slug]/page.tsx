4f84ac484ec0c29fc2e69440a014ab2d2769644e
Chen Peleg <chenpeleg@gmail.com>
2025-12-07 10:31:00 +0200

    feat: replace img tag with Image component for optimized loading in PostsPage

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React  from 'react';
 import {PageProperties} from '@/models/PageProperties';
 import dumplings from '@/assets/images/dumplings.jpg';
+import {Image} from '@/node_modules/next/dist/client/image-component';
 
 
 export default function PostsPage(props: PageProperties<'/posts/[slug]'>) {
@@ -8,7 +9,7 @@ export default function PostsPage(props: PageProperties<'/posts/[slug]'>) {
 
     return (<div>
         <h1> {postName.slug}  </h1>
-        <img src={dumplings.src} alt={'dumplings'} />
+        <Image src={dumplings } alt={'dumplings'}/>
         <p>This is the posts page content.</p>
     </div>);
 }
