236e112d4cdb185653256ff50c1b930ca98788fc
Chen Peleg <chenpeleg@gmail.com>
2025-12-07 08:55:06 +0200

    feat: remove unnecessary blank line in PostsPage component

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -5,7 +5,6 @@ import {PageProperties} from '@/models/PageProperties';
 export default function PostsPage(props: PageProperties<'/posts/[slug]'>) {
     const postName = React.use(props.params);
 
-
     return (<div>
         <h1> {postName.slug}  </h1>
         <p>This is the posts page content.</p>
