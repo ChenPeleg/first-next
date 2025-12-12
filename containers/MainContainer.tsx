3d3c94f4248dd9340781d31bfd270c1780364ff1
Chen Peleg <chenpeleg@gmail.com>
2025-12-12 11:23:44 +0200

    feat: adjust MainContainer layout to ensure full height for main and section elements

--- containers/MainContainer.tsx
+++ containers/MainContainer.tsx
@@ -7,9 +7,9 @@ export const MainContainer = ({ children ,header }:
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             {header}
             <main
-                className="flex min-h-screen w-full flex-row items-center justify-center bg-green-300 gap-10 py-10     dark:bg-black sm:items-start">
+                className="flex min-h-screen w-full h-full flex-row items-center justify-center bg-green-300 gap-10 py-10     dark:bg-black sm:items-start">
 
-                <section className="flex  max-w-3xl w-full   bg-amber-300 flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                <section className="flex  max-w-3xl w-full h-full bg-amber-300 flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     {children}
                 </section>
             </main>
